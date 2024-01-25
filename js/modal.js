879d98c7ac58bec0b08f27318bbab76258e3b310
estebsuam11 <xseyer.suarez@gmail.com>
2024-01-24 22:45:09 -0500

    arreglo modal

--- js/modal.js
+++ js/modal.js
@@ -1,12 +1,10 @@
-const openModals = document.querySelectorAll('.hero__cta');
+const openModal = document.querySelector('.hero__cta');
 const modal = document.querySelector('.modal');
 const closeModal = document.querySelector('.modal__close');
 
-openModals.forEach(function (openModal) {
-    openModal.addEventListener('click', function (e) {
-        e.preventDefault();
-        modal.classList.add('modal--show');
-    });
+openModal.addEventListener('click', (e)=>{
+    e.preventDefault();
+    modal.classList.add('modal--show');
 });
 
 
